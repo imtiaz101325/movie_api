02c65a77b7daff451be708c772d01ce6a04c26b7
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-04-05 14:13:31 +0600

    Refactored to items and  items loaders

--- wikipedia_scraper/wikipedia_scraper/items.py
+++ wikipedia_scraper/wikipedia_scraper/items.py
@@ -5,10 +5,40 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Field, Item
+
+class MovieItem(Item):
+    name = Field()
+    image = Field()
+    year = Field()
+    awards = Field()
+    nominations = Field()
+    directed_by = Field()
+    produced_by = Field()
+    screenplay_by = Field()
+    story_by = Field()
+    starring = Field()
+    music_by = Field()
+    cinematography = Field()
+    edited_by = Field()
+    production = Field()
+    distributed_by = Field()
+    release_date = Field()
+    running_time = Field()
+    country = Field()
+    language = Field()
+    budget = Field()
+    box_office = Field()
+    based_on = Field()
+    written_by = Field()
+    animation_by = Field()
+    layouts_by = Field()
+    color_process = Field()
+    narrated_by = Field()
+    backgrounds_by = Field()
 
 
-class WikipediaScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ImageItem(Item):
+    src = Field()
+    srcset = Field()
+    alt_text = Field()
--- wikipedia_scraper/wikipedia_scraper/spiders/movies.py
+++ wikipedia_scraper/wikipedia_scraper/spiders/movies.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import logging
+from scrapy.loader import ItemLoader
+from wikipedia_scraper.items import MovieItem, ImageItem
 
 class MoviesSpider(scrapy.Spider):
     name = 'movies'
@@ -8,7 +9,7 @@ class MoviesSpider(scrapy.Spider):
     start_urls = ['https://en.wikipedia.org/wiki/List_of_Academy_Award-winning_films']
 
     def parse(self, response):
-        table_rows = response.css('.wikitable tr')[1:][:5]
+        table_rows = response.css('.wikitable tr')[1:][:2]
         for row in table_rows:
             cells = row.css('td')
             link = cells[0].css('a')[0]
@@ -17,20 +18,36 @@ class MoviesSpider(scrapy.Spider):
                 for cell in cells
             ]
 
-            details = {
-                'award_page_name': award_data[0],
-                'year': award_data[1],
-                'awards': award_data[2],
-                'nominations': award_data[3]
-            }
+            loader = ItemLoader(item=MovieItem())
+            loader.add_value('year', award_data[1])
+            loader.add_value('awards', award_data[2])
+            loader.add_value('nominations', award_data[3])
+            movie_item = loader.load_item()
 
-            yield response.follow(link, self.parse_movie_links, cb_kwargs={ 'details': details })
+            yield response.follow(link, self.parse_movie_links, cb_kwargs={ 'movie_item': movie_item })
 
-    def parse_movie_links(self, response, details):
-        infoboxRows = response.css('table.infobox')[0].css('tr')        
-        movie = {
-            **details
-        }
+    def parse_movie_links(self, response, movie_item):
+        infoboxRows = response.css('table.infobox')[0].css('tr')
+        loader = ItemLoader(movie_item, selector=infoboxRows)
+
+        loader.add_css('name', '.summary::text')
+
+        image_loader = ItemLoader(item=ImageItem(), selector=infoboxRows.css('a.image img'))
+        image_loader.add_css('src', '::attr(src)')
+        image_loader.add_css('srcset', '::attr(srcset)')
+        image_loader.add_css('alt_text', '::attr(alt)')
+        loader.add_value('image', image_loader.load_item())
+        
+        movie_item_fields = [
+            'name', 'image', 'year', 'awards',
+            'nominations', 'directed_by', 'produced_by',
+            'screenplay_by', 'story_by', 'starring', 'music_by',
+            'cinematography', 'edited_by', 'production', 'distributed_by',
+            'release_date', 'running_time', 'country', 'language',
+            'budget', 'box_office', 'based_on', 'written_by',
+            'animation_by', 'layouts_by', 'color_process', 'narrated_by',
+            'backgrounds_by'
+        ]
 
         for row in infoboxRows:
             header = row.css('th')
@@ -38,35 +55,21 @@ class MoviesSpider(scrapy.Spider):
 
             header_text = header.css('::text').get()
             header_text = header_text and header_text.strip()
-            details_text = details.css('::text').get()
-            details_text = details_text and details_text.strip()
+            header_as_key = header_text and '_'.join(header_text.lower().split(' '))
 
-            if header_text and details_text:
-                movie[header_text.strip()] = details_text.strip()
-            
-            name = header.css('.summary::text').get()
-            if name:
-                movie['name'] = name
+            value = None
 
-            image = details.css('a.image img')
-            if image.get():
-                srcset = image.css('::attr(srcset)').get()
-                src = image.css('::attr(src)').get()
-                alt_text = image.css('::attr(alt)').get()
-                movie['image'] = {
-                    'src': src,
-                    'srcset': srcset,
-                    'alt_text': alt_text
-                }
-            
-            #TODO: fix broken text
             plainlist = details.css('.plainlist')
             if plainlist.get() and header_text != 'Release date':
-                movie[header_text] = plainlist.css('li::text').getall()
-                movie[header_text] += plainlist.css('li > a::text').getall()
-            
-            
-            if header_text == 'Release date':
-                movie[header_text] = plainlist.css('li .published::text').getall()
-        
-        yield movie
+                value = plainlist.css('li::text').getall()
+                value += plainlist.css('li > a::text').getall()
+            elif header_text == 'Release date':
+                value = plainlist.css('li .published::text').getall()
+            else:
+                value = details.css('::text').get()
+                value = value and value.strip()
+
+            if header_as_key in movie_item_fields:
+                loader.add_value(header_as_key, value)
+
+        yield loader.load_item()
